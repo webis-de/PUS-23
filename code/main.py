42e12271ba9a96877448c7b34a9d1ff70913a382
Wolfgang Kircheis <wolfgang_kircheis@web.de>
2020-07-08 01:36:32 +0200

    Add phrases
    Former-commit-id: 9bc722128407025935c562003d0299fd3b727f9d

--- code/main.py
+++ code/main.py
@@ -14,12 +14,18 @@ bibliography.plot_publication_distribution_to_file(output_directory)
 article = Article(".." + sep + "data" + sep + "CRISPR_en.xml.xz")
 article.plot_revision_distribution_to_file(output_directory)
 
-#extract tracks of bibkey values in article
-bibkey_tracks = article.track_bibkeys_in_article(["titles", "dois", "authors"], bibliography)
-#phrase_tracks = article.track_phrases_in_article([["gene editing", "crispr cas", "medicine", "cancer", "science"]])
+phrases = ["adaptive_immunity","agriculture","application","bolotin","broad_institute","cas_9","controversy",
+           "create_new_species","crispr","crispr_cas","crispr_cas_9","crispr_locus","crispr_rnas","crrnas","disease",
+           "dna","double_stranded","doudna","e_coli","embryos","ethics","gene edit","gmos","guide_rna","human","marraffini",
+           "max_planck_institute","medicine","moineau","mojica","pam","patent","patient","s_thermophilus","sontheimer","talens",
+           "technical","technique","technology","tool","tracrrna","type_ii","u_california","u_vienna","upstream","van_der_oost","zhang","zinc"]
+
+#extract tracks of bibkey values or phrase lists in article
+#tracks = article.track_bibkeys_in_article(["titles", "dois", "authors"], bibliography)
+tracks = article.track_phrases_in_article([phrases])
 
 #write and plot results to file
-for track in bibkey_tracks.items():
+for track in tracks.items():
     article.write_track_to_file(track, output_directory)
     article.plot_track_to_file(track, output_directory)
 
