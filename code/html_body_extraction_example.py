7c7450c65b17f780c50372f42a94d9b713362081
kircheis <wolfgang.kircheis@uni-leipzig.de>
2020-09-22 11:08:53 +0200

    Update html_body_extraction_example.py
    Former-commit-id: 19375e8ed2f1945ac285b2d05c58620ee8cb75dc

--- code/html_body_extraction_example.py
+++ code/html_body_extraction_example.py
@@ -1,22 +1,37 @@
 from requests import get
 from lxml import etree
 from re import sub
+from os import remove, system
+from time import sleep
 
 revisions = {"CRISPR_de_first_rev":"https://de.wikipedia.org/w/index.php?title=CRISPR&oldid=69137443",
-             "CRISPR_en_first_rev":"https://en.wikipedia.org/w/index.php?title=CRISPR&oldid=17918488"}
+             "CRISPR_en_first_rev":"https://en.wikipedia.org/w/index.php?title=CRISPR&oldid=17918488",
+             "CRISPR_en_remov_rev":"https://en.wikipedia.org/w/index.php?title=CRISPR&oldid=898138824"}
 
 def extract_article_body(url, name):
-    res = get(url)
+    system("firefox '" + url + "'")
+    
+    html = get(url).text
 
-    tree   = etree.HTML(res.text)
+    tree   = etree.HTML(html)
 
-    result = etree.tostring(tree.findall(".//div[@class='mw-parser-output']")[0])
+    try:
+        content = etree.tostring(tree.findall(".//div[@class='mw-parser-output']")[0])
+        cleaned_content = content.decode("utf-8").replace("\n","")
+        result = sub(r"<!--.*-->","", cleaned_content)
+    except IndexError:
+        result = ""
 
-    string = result.decode("utf-8").replace("\n","")
-    string = sub(r"<!--.*-->","", string)
+    filename = name + ".html"
+    
+    with open(filename, "w") as file:
+        file.write(result)
 
-    with open(name + ".html", "w") as file:
-        file.write(string)
+    system("firefox " + filename)
+
+    sleep(0.5)
+
+    remove(filename)
 
 for revision in revisions.items():
     extract_article_body(revision[1], revision[0])
