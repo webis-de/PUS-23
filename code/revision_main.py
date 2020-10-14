213945fa49055c70f8a695e856df75ec9e7f32b8
kircheis <wolfgang.kircheis@uni-leipzig.de>
2020-10-14 23:11:59 +0200

    Update
    Former-commit-id: 51921f88eb84389226149940e440462cea07ee91

--- code/revision_main.py
+++ code/revision_main.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     LANGUAGE = "en"
 
     #Open scraped article and get random revision.
-    random_index = randint(0,2023)
+    random_index = 1935#randint(0,2023)
     line = 0
     with open("../extractions/CRISPR_" + LANGUAGE) as article:
         while line < random_index:
@@ -38,7 +38,28 @@ if __name__ == "__main__":
 
     #Print paragraphs from html
     heading("\nPARAGRAPHS")
-    print(sub(r"\n\n+", "\n\n", "\n\n".join([paragraph.text() for paragraph in revision.get_paragraphs()])))
+    paragraphs = revision.get_paragraphs()
+    print(sub(r"\n\n+", "\n\n", "\n\n".join([section.text() for section in paragraphs])))
+
+    #Print headings from html
+    heading("\nHEADINGS")
+    headings = revision.get_headings()
+    print(sub(r"\n\n+", "\n\n", "\n\n".join([section.text() for section in headings])))
+
+    #Print lists from html
+    heading("\nLISTS")
+    lists = revision.get_lists()
+    print(sub(r"\n\n+", "\n\n", "\n\n".join([section.text() for section in lists])))
+
+    #Print captions from html
+    heading("\nCAPTIONS")
+    captions = revision.get_captions()
+    print(sub(r"\n\n+", "\n\n", "\n\n".join([section.text() for section in captions])))
+
+    #Print tables from html
+    heading("\nTABLES")
+    tables = revision.get_tables()
+    print(sub(r"\n\n+", "\n\n", "\n\n".join([section.text() for section in tables])))
 
     #Print all categories.
     heading("\nCATEGORIES")
@@ -48,6 +69,7 @@ if __name__ == "__main__":
     #Print references and further reading from html.
     CITATION_STYLE = LANGUAGE #citation style different for German (de) and English (en)
     sources = {"REFERENCES": revision.get_references(), "FURTHER READING":revision.get_further_reading()}
+    sections = paragraphs + headings + lists + captions + tables
     for source in sources.items():
         authors = revision.get_referenced_authors(CITATION_STYLE, source[1])
         titles = revision.get_referenced_titles(CITATION_STYLE, source[1])
@@ -63,5 +85,7 @@ if __name__ == "__main__":
             print("\nLINKED PARAGRAPHS:")
             backlinks = reference.backlinks()
             if backlinks:
-                print("\n".join(["=>" + str(backlink) + "\n" + linked_paragraph.text().strip() for backlink, linked_paragraph in zip(backlinks, reference.linked_paragraphs(revision.get_paragraphs()))]))
+                print("\n".join(["=>" + str(backlink) + "\n" + linked_section.text().strip() for backlink, linked_section in zip(backlinks, reference.linked_sections(sections))]))
+            else:
+                print("-")
             input("-"*50)
