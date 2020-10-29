ee3266b123bb7baa473311dcb20bd8c9d9908676
kircheis <wolfgang.kircheis@uni-leipzig.de>
2020-10-29 22:30:08 +0100

    Add powerset
    Former-commit-id: 0289eac1b67aab29e71d5649bc11881a581ffa2d

--- code/utility/utils.py
+++ code/utility/utils.py
@@ -11,6 +11,28 @@ def flatten_list_of_lists(list_of_lists):
     """
     return [item for sublist in list_of_lists for item in sublist]
 
+def powerset(items, result = [], subset = [], min_len_sub = 1):
+    """
+    Calculate the powerset of subsets of given list.
+
+    Args:
+        items: The list for which the powerset should be calculated.
+        result: The resulting powerset.
+        min_len_sub: The minimum length of subsets to return.        
+    """
+    if len(items) + len(subset) < min_len_sub:
+        return
+    elif not items:
+        result.append(set(subset))
+    else:
+        powerset(items[1:], result, subset + [items[0]], min_len_sub)
+        powerset(items[1:], result, subset, min_len_sub)
+    return sorted(result, key = lambda x: len(x))
+
 if __name__ == "__main__":
 
-    print(flatten_list_of_lists([[1,2,3],["a","b","c"]]))
+    print(flatten_list_of_lists([[1,2,3],["a","b","c","d","e"]]))
+
+    items = list(["a","b","c","d","e"])
+
+    print(powerset(items, min_len_sub = 2))
