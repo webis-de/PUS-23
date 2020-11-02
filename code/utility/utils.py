397bc1118ce5c0a908068c71eb832251a6d49787
kircheis <wolfgang.kircheis@uni-leipzig.de>
2020-11-02 13:21:52 +0100

    Update powerset
    Former-commit-id: 69c62f3ded984df8fb8e06931d96764472d1af9d

--- code/utility/utils.py
+++ code/utility/utils.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 def flatten_list_of_lists(list_of_lists):
     """
     Flattens list of lists to one list, i.e.
@@ -11,28 +13,90 @@ def flatten_list_of_lists(list_of_lists):
     """
     return [item for sublist in list_of_lists for item in sublist]
 
-def powerset(items, result = [], subset = [], min_len_sub = 1):
+def calc_powerset(items, result = [], subset = [], min_len_sub = 1):
     """
     Calculate the powerset of subsets of given list.
 
     Args:
         items: The list for which the powerset should be calculated.
         result: The resulting powerset.
-        min_len_sub: The minimum length of subsets to return.        
+        subset: Holds subset during recursion.
+        min_len_sub: The minimum length of subsets to return.
+    Returns:
+        The powerset.
     """
     if len(items) + len(subset) < min_len_sub:
-        return
+        return ()
     elif not items:
-        result.append(set(subset))
+        result.append(tuple(subset))
     else:
-        powerset(items[1:], result, subset + [items[0]], min_len_sub)
-        powerset(items[1:], result, subset, min_len_sub)
-    return sorted(result, key = lambda x: len(x))
+        calc_powerset(items[1:], result, subset + [items[0]], min_len_sub)
+        calc_powerset(items[1:], result, subset, min_len_sub)
+    return result
+
+def powerset1(items, min_len_sub):
+    """
+    Calculate the powerset of given list.
+
+    Args:
+        items: The list for which the powerset should be calculated.
+        min_len_sub: The minimum length of subsets to return.
+    Returns:
+        The powerset.
+    """
+    return calc_powerset(items, [], [], min_len_sub)
+
+def powerset2(items, min_len):
+    """
+    Calculate the powerset of given list.
+
+    Args:
+        items: The list for which the powerset should be calculated.
+    Returns:
+        The powerset.
+    """
+    return [x for length in range(len(items)+1, min_len-1, -1) for x in combinations(items, length)]
+
+def powerset3(items, min_len):
+    """
+    Calculate the powerset of given list.
+
+    Args:
+        items: The list for which the powerset should be calculated.
+    Returns:
+        The powerset.
+    """
+    return [x for length in range(len(items)+1, min_len-1, -1) for x in my_combinations(items, length, [], [])]
+
+def my_combinations(items, r, result, subset):
+    if r > len(items):
+        result.append(subset)
+    else:
+        for i in range(r):
+            my_combinations(items[i+1:], r-i, result, subset + [items[i]])
+    return result
 
 if __name__ == "__main__":
 
+    from datetime import datetime
+
+    #print(comb([1,2,3,4,5],[],[],4))
+
     print(flatten_list_of_lists([[1,2,3],["a","b","c","d","e"]]))
+    
+    items = list([1,2,3,4,5,6,7,8,9,10,11,12,13])
 
-    items = list(["a","b","c","d","e"])
+    start = datetime.now()
+    ps1 = powerset1(items, min_len_sub = 0)
+    print(ps1)
+    print(datetime.now() - start)
 
-    print(powerset(items, min_len_sub = 2))
+    start = datetime.now()
+    ps2 = powerset2(items, min_len = 0)
+    print(ps2)
+    print(datetime.now() - start)
+
+    start = datetime.now()
+    ps3 = powerset3(items, min_len = 0)
+    print(ps3)
+    print(datetime.now() - start)
