c55342bd8261c3fbe7f06a201cb5f11928f41e26
kircheis <wolfgang.kircheis@uni-leipzig.de>
2021-01-12 11:34:34 +0100

    Update events.csv and main_timeline.py
    Former-commit-id: 8c15ae27e41b50d758c1d254e98dbb000f3b00f8

--- code/main_timeline.py
+++ code/main_timeline.py
@@ -3,20 +3,50 @@ from timeline.accountlist import AccountList
 from timeline.eventlist import EventList
 from pprint import pprint
 
-##################################################################
-# This file serves as an entry point view the event and accounts.#
-##################################################################
+###############################################################
+# This file serves as an entry point view events and accounts.#
+###############################################################
 
-el = EventList("../data/CRISPR_events - events.csv",
-               Bibliography("../data/tracing-innovations-lit.bib"),
-               AccountList("../data/CRISPR_events - accounts.csv"))
+def heading(text):
+    print(text)
+    print("="*len(text))
+    print()
 
-event_types = [event.type for event in el.events]
-event_types = {event_type:event_types.count(event_type) for event_type in set(event_types)}
+if __name__ == "__main__":
 
-pprint(event_types)
+    bibliography = Bibliography("../data/tracing-innovations-lit.bib")
+    accountlist = AccountList("../data/CRISPR_events - accounts.csv")
+    eventlist = EventList("../data/CRISPR_events - events.csv", bibliography, accountlist)
 
-print("="*50)
-print(el.events[124])
-print("="*50)
-pprint(el.events[124].json())
+    event_types = [event.type for event in eventlist.events]
+    event_types = {event_type:event_types.count(event_type) for event_type in set(event_types)}
+
+    heading("\nEVENT TYPES")
+    pprint(event_types)
+
+    conditions = ["event.type=='publication'",
+              "event.extracted_from!='narrative_structure'",
+              "not(event.extracted_from=='timeline_structure' and event.account_id in ['2','3','4'])"]
+
+    event_count = 0
+
+    for event in eventlist.events:
+        for condition in conditions:
+            if not eval(condition):
+                break
+        else:
+            event_count += 1
+
+    heading("\nEVENT COUNT")
+    print(len(eventlist.events), "(all)\n")
+    print(event_count, "with conditions:")
+
+    print("".join(["\n - " + condition for condition in conditions]))
+
+    if False:
+    
+        heading("\nEVENT 124 (STRING)")
+        print(eventlist.events[124])
+        
+        heading("\nEVENT 124 (JSON)")
+        pprint(eventlist.events[124].json())
