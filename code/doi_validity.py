9341f6b1b507e868378448da87134aa73eb3316f
kircheis <wolfgang.kircheis@uni-leipzig.de>
2021-01-25 14:37:23 +0100

    Update doi_validity.py
    Former-commit-id: 6f521f8cf3506494dd7256838391e6e6cabbe977

--- code/doi_validity.py
+++ code/doi_validity.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
 
     with open("doi_validaity.txt", "w") as file:
         for bib in bibliography.bibentries.values():
-            file.write(bib.bibkey + "\n")
-            if not bib.doi_valid():
-                file.write("\n" + str(bib.__dict__) + "\n\n")
+            if bib.doi:
+                file.write(bib.bibkey + "\n")
+                if not bib.doi_valid():
+                    file.write("\n" + str(bib.__dict__) + "\n\n")
     
