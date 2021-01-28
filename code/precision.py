b072f4c78e0a184c7a916a4ce28ba3705b048083
kircheis <wolfgang.kircheis@uni-leipzig.de>
2021-01-29 00:58:38 +0100

    Update precision.py
    Former-commit-id: 313257775781079091e8aeb2e4dcb2fa2134417e

--- code/precision.py
+++ code/precision.py
@@ -2,7 +2,7 @@ from json import load, dumps
 from os.path import basename, dirname, sep
 from pprint import pprint
 
-json_path = "../analysis/FOOBAR/2021_01_28_18_35_00/CRISPR_de.json"
+json_path = "../analysis/2021_01_25_14_44_39/CRISPR_en.json"
 
 with open(json_path) as file:
     data = load(file)
@@ -15,21 +15,31 @@ for event in data:
             precisions[key] = [0,0]
         if event["first_mentioned"]["relaxed"][key]:
             bibkey = list(event["bibentries"].keys())[0]
+            title = event["bibentries"][bibkey]["title"]
+            authors = event["bibentries"][bibkey]["authors"]
+            doi = event["bibentries"][bibkey]["doi"]
+            pmid = event["bibentries"][bibkey]["pmid"]
+            year = event["bibentries"][bibkey]["year"]
+            try:
+                journal = event["bibentries"][bibkey]["journal"]
+            except KeyError:
+                journal = None
             print("="*50)
             print("BIBLIOGRAPH ENTRY:")
-            print("Title:", event["bibentries"][bibkey]["title"])
-            print("Authors:",event["bibentries"][bibkey]["authors"])
-            print("DOI:",event["bibentries"][bibkey]["doi"])
-            print("PMID:",event["bibentries"][bibkey]["pmid"])
-            try:
-                print("Journal:",event["bibentries"][bibkey]["journal"])
-            except KeyError:
-                pass
+            print("Title:", title)
+            print("Authors:", authors)
+            print("DOI:", doi)
+            print("PMID:", pmid)
+            print("Year:", year)
+            print("Journal:", journal)
             print()
+            match = event["first_mentioned"]["relaxed"][key]["result"][bibkey]["source_text"]["raw"]
             print("MATCH:")
-            print(event["first_mentioned"]["relaxed"][key]["result"][bibkey]["source_text"]["raw"])
-            print()
-            correct = input("PRESS ENTER IF CORRECT, ENTER ANY OTHER KEY AND PRESS ENTER IF INCORRECT. ")
+            print(match)
+            if match and ((title and title in match) or (doi and doi in match) or (pmid and pmid in match)):
+                correct = True
+            else:
+                correct = input("\nPRESS ENTER IF CORRECT, ENTER ANY OTHER KEY AND PRESS ENTER IF INCORRECT. ")
             if correct:
                 precisions[key][0] += 1
             precisions[key][1] += 1
