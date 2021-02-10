21240f891c8b02fbe186d4cbca161e21c4ba39aa
kircheis <wolfgang.kircheis@uni-leipzig.de>
2021-02-10 19:39:12 +0100

    Update occurrence calculation, table writing and plotting
    Former-commit-id: 9c24a9ec8b351489c9c5da6fd4f410d0c8824282

--- code/evaluation/occurrence.py
+++ code/evaluation/occurrence.py
@@ -24,7 +24,7 @@ def stringify_delay(delay):
     else:
         return str(int(delay - ZERO))
 
-def calculate_and_write_occurrence_table(json_path, sort):
+def occurrence(json_path, sort):
 
     with open(json_path) as file:
         data = [event for event in load(file) if event["bibentries"]]
@@ -103,122 +103,77 @@ def calculate_and_write_occurrence_table(json_path, sort):
                     file.write("--- Relaxed Match Measures ---" + "\n")
             file.write("\n")
 
-def calculate_delays_and_write_table_and_plot(json_file, skip_no_result):
+def calculate_delays(json_file, skip_no_result):
 
-    data = sorted([event for event in load(open(json_file)) if event["bibentries"]], key=lambda event: int(event["event_year"]))
+    #select events with bibentries and (if skip_no_result) with matches, sort by event_year
+    data = sorted([event for event in load(open(json_file)) if event["bibentries"] and (any(list(event["first_mentioned"]["verbatim"].values()) + list(event["first_mentioned"]["relaxed"].values())) if skip_no_result else True)],
+                  key=lambda event: int(event["event_year"]))
 
-    bibkey_list = [list(event["bibentries"].keys())[0] for event in data]
-    event_year_list = [int(event["event_year"]) if event["event_year"] else None for event in data]
+    for event in data:
+        for strategy in ["verbatim", "relaxed"]:
+            for method in event["first_mentioned"][strategy]:
+                if event["first_mentioned"][strategy][method]:
+                    event["first_mentioned"][method] = calculate_delay(int(event["first_mentioned"][strategy][method]["timestamp"][:4]),int(event["event_year"]))
+                else:
+                    event["first_mentioned"][method] = calculate_delay(None,int(event["event_year"]))
+            del event["first_mentioned"][strategy]
 
-    verbatim_title_list = [int(event["first_mentioned"]["verbatim"]["titles"]["timestamp"][:4]) if event["first_mentioned"]["verbatim"]["titles"] else None for event in data]
-    verbatim_doi_list = [int(event["first_mentioned"]["verbatim"]["dois"]["timestamp"][:4]) if event["first_mentioned"]["verbatim"]["dois"] else None for event in data]
-    verbatim_pmid_list = [int(event["first_mentioned"]["verbatim"]["pmids"]["timestamp"][:4]) if event["first_mentioned"]["verbatim"]["pmids"] else None for event in data]
-    ned_02_list = [int(event["first_mentioned"]["relaxed"]["ned <= 0.2"]["timestamp"][:4]) if event["first_mentioned"]["relaxed"]["ned <= 0.2"] else None for event in data]
-    ned_03_list = [int(event["first_mentioned"]["relaxed"]["ned <= 0.3"]["timestamp"][:4]) if event["first_mentioned"]["relaxed"]["ned <= 0.3"] else None for event in data]
-    ned_04_list = [int(event["first_mentioned"]["relaxed"]["ned <= 0.4"]["timestamp"][:4]) if event["first_mentioned"]["relaxed"]["ned <= 0.4"] else None for event in data]
-    ned_and_ratio_list = [int(event["first_mentioned"]["relaxed"]["ned_and_ratio"]["timestamp"][:4]) if event["first_mentioned"]["relaxed"]["ned_and_ratio"] else None for event in data]
-    ned_and_jaccard_list = [int(event["first_mentioned"]["relaxed"]["ned_and_jaccard"]["timestamp"][:4]) if event["first_mentioned"]["relaxed"]["ned_and_jaccard"] else None for event in data]
-    ned_and_skat_list = [int(event["first_mentioned"]["relaxed"]["ned_and_skat"]["timestamp"][:4]) if event["first_mentioned"]["relaxed"]["ned_and_skat"] else None for event in data]
+    return data
 
-    original_lists = [bibkey_list, event_year_list, verbatim_title_list, verbatim_doi_list, verbatim_pmid_list, ned_02_list, ned_03_list, ned_04_list, ned_and_ratio_list, ned_and_jaccard_list, ned_and_skat_list]
+def write_delay_table(json_file, data, skip_no_result):
 
-    if skip_no_result:
-        value_lists = original_lists[2:]
-        reduced_lists = []
-
-        for original_list in original_lists:
-            reduced_list = []
-            for i in range(len(original_list)):
-                if len([value_list[i] for value_list in value_lists if value_list[i]]) > 0:
-                    reduced_list.append(original_list[i])
-            reduced_lists.append(reduced_list)
-
-        for i in range(len(original_lists)):
-            original_lists[i] = reduced_lists[i]
-
-    bibkey_list = original_lists[0]
-    event_year_list = original_lists[1]
-    verbatim_title_delays = [calculate_delay(match, year) for match,year in zip(original_lists[2], event_year_list)]
-    verbatim_doi_delays = [calculate_delay(match, year) for match,year in zip(original_lists[3], event_year_list)]
-    verbatim_pmid_delays = [calculate_delay(match, year) for match,year in zip(original_lists[4], event_year_list)]
-    ned_02_delays = [calculate_delay(match, year) for match,year in zip(original_lists[5], event_year_list)]
-    ned_03_delays = [calculate_delay(match, year) for match,year in zip(original_lists[6], event_year_list)]
-    ned_04_delays = [calculate_delay(match, year) for match,year in zip(original_lists[7], event_year_list)]
-    ned_and_ratio_delays = [calculate_delay(match, year) for match,year in zip(original_lists[8], event_year_list)]
-    ned_and_jaccard_delays = [calculate_delay(match, year) for match,year in zip(original_lists[9], event_year_list)]
-    ned_and_skat_delays = [calculate_delay(match, year) for match,year in zip(original_lists[10], event_year_list)]
+    header = ["bibkey","event_year","verbatim_title","verbatim_doi","verbatim_pmid","ned <= 0.2","ned <= 0.3","ned <= 0.4","ned_and_ratio","ned_and_jaccard","ned_and_skat"]
 
     with open(dirname(json_file) + sep + "occurrence" + "_by_bibkey" + ("_all" if not skip_no_result else "") + ".csv", "w") as file:
-        file.write("bibkey" + "," + \
-                   "event_year" + "," + \
-                   "verbatim_title" + "," + \
-                   "verbatim_doi" + "," + \
-                   "verbatim_pmid" + "," + \
-                   "ned <= 0.2" + "," + \
-                   "ned <= 0.3" + "," + \
-                   "ned <= 0.4" + "," + \
-                   "ned_and_ratio" + "," + \
-                   "ned_and_jaccard" + "," + \
-                   "ned_and_skat" + "\n")
+        file.write(",".join(header) + "\n")
 
-        for bibkey, event_year, verbatim_title_delay, verbatim_doi_delay, verbatim_pmid_delay, ned_02_delay, ned_03_delay, ned_04_delay, ned_and_ratio_delay, ned_and_jaccard_delay, ned_and_skat_delay in \
-            zip(bibkey_list, event_year_list, verbatim_title_delays, verbatim_doi_delays, verbatim_pmid_delays, ned_02_delays, ned_03_delays, ned_04_delays, ned_and_ratio_delays, ned_and_jaccard_delays, ned_and_skat_delays):
-            file.write(str(bibkey) + "," + \
-                       str(event_year) + "," + \
-                       stringify_delay(verbatim_title_delay) + "," + \
-                       stringify_delay(verbatim_doi_delay) + "," + \
-                       stringify_delay(verbatim_pmid_delay) + "," + \
-                       stringify_delay(ned_02_delay) + "," + \
-                       stringify_delay(ned_03_delay) + "," + \
-                       stringify_delay(ned_04_delay) + "," + \
-                       stringify_delay(ned_and_ratio_delay) + "," + \
-                       stringify_delay(ned_and_jaccard_delay) + "," + \
-                       stringify_delay(ned_and_skat_delay) + "\n")
+        for event in data:
+            file.write(list(event["bibentries"].keys())[0] + "," + \
+                       str(event["event_year"]) + "," + \
+                       stringify_delay(event["first_mentioned"]["titles"]) + "," + \
+                       stringify_delay(event["first_mentioned"]["dois"]) + "," + \
+                       stringify_delay(event["first_mentioned"]["pmids"]) + "," + \
+                       stringify_delay(event["first_mentioned"]["ned <= 0.2"]) + "," + \
+                       stringify_delay(event["first_mentioned"]["ned <= 0.3"]) + "," + \
+                       stringify_delay(event["first_mentioned"]["ned <= 0.4"]) + "," + \
+                       stringify_delay(event["first_mentioned"]["ned_and_ratio"]) + "," + \
+                       stringify_delay(event["first_mentioned"]["ned_and_jaccard"]) + "," + \
+                       stringify_delay(event["first_mentioned"]["ned_and_skat"]) + "\n")
 
-        if not skip_no_result:
-            file.write("\n")
-            file.write("OCCURRENCE,")
-            for i in range(2, len(original_lists)):
-                file.write("," + str(round(len([item for item in original_lists[i] if item])/len(original_lists[i])*100, 2)))
-            
+def plot_delays(json_file, data, methods):
 
-    if skip_no_result:
-        event_year_list = [str(year) for year in original_lists[1]]
-        '''
-        year = event_year_list[0]
-        for i in range(1, len(event_year_list)):
-            if event_year_list[i] == year:
-                event_year_list[i] = ""
-            else:
-                year = event_year_list[i]
-        '''
+    data_map = {method:[] for method in methods}
 
-        width = 0.08
-        x = np.arange(len(bibkey_list))
-        plt.figure(figsize=(60, 12.5), dpi=200)
-        plt.subplots_adjust(bottom=0.15, top=0.99, left=0.01, right=0.998)
-        plt.margins(x=0.0005, y=0.005)
-        plt.xticks(np.arange(len(bibkey_list)), bibkey_list, rotation=90)
-        plt.xlabel("PUBLICATIONS\n(colours represent different matching strategies as per legend | column hints of 0.1 represent no match or false positive (negative delay) | all matches +0.25 to visualise same-year occurrence)")
-        plt.ylabel("OCCURRENCE DELAY IN YEARS")
-        plt.bar(x - width*4, verbatim_title_delays, width=width, label="verbatim_title")
-        plt.bar(x - width*3, verbatim_doi_delays, width=width, label="verbatim_doi")
-        plt.bar(x - width*2, verbatim_pmid_delays, width=width, label="verbatim_pmid")
-        plt.bar(x - width*1, ned_02_delays, width=width, label="ned <= 0.2")
-        plt.bar(x + width*0, ned_03_delays, width=width, label="ned <= 0.3")
-        plt.bar(x + width*1, ned_04_delays, width=width, label="ned <= 0.4")
-        plt.bar(x + width*2, ned_and_ratio_delays, width=width, label="ned_and_ratio")
-        plt.bar(x + width*3, ned_and_jaccard_delays, width=width, label="ned_and_jaccard")
-        plt.bar(x + width*4, ned_and_skat_delays, width=width, label="ned_and_skat")
-        plt.legend()
-        plt.savefig(dirname(json_file) + sep + "delays.png")
+    for event in data:
+        for method in methods:
+            data_map[method].append(event["first_mentioned"][method])
+
+    width = 0.8 / len(methods)
+    x = np.arange(len(data))
+    plt.figure(figsize=(60, 12.5), dpi=200)
+    plt.subplots_adjust(bottom=0.15, top=0.99, left=0.01, right=0.998)
+    plt.margins(x=0.0005, y=0.005)
+    plt.xticks(np.arange(len(data)), [list(event["bibentries"].keys())[0] for event in data], rotation=90)
+    plt.xlabel("PUBLICATIONS\n(colours represent different matching strategies as per legend | column hints of 0.05 represent no match or false positive (negative delay) | all matches +0.25 to visualise same-year occurrence)")
+    plt.ylabel("OCCURRENCE DELAY IN YEARS")
+    i = - int(len(methods) / 2)
+    for method,delays in data_map.items():
+        plt.bar(x + width*i, delays, width=width, label=method)
+        i += 1
+    plt.legend()
+    plt.savefig(dirname(json_file) + sep + "delays.png")
 
 if __name__ == "__main__":
 
     from path import json_path
-    
-    calculate_and_write_occurrence_table(json_path, False)
-    calculate_delays_and_write_table_and_plot(json_path, True)
-    calculate_delays_and_write_table_and_plot(json_path, False)
 
+    occurrence(json_path, False)
 
+    delays = calculate_delays(json_path, True)
+    delays_all = calculate_delays(json_path, False)
+
+    write_delay_table(json_path, delays, True)
+    write_delay_table(json_path, delays_all, False)
+
+    methods = ["titles","dois","pmids"]
+    plot_delays(json_path, delays, methods)
