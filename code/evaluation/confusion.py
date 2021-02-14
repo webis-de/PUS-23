bbc68f48fe0a6107fc38a40ac55fd2743c950618
kircheis <wolfgang.kircheis@uni-leipzig.de>
2021-02-14 23:58:29 +0100

    Add size vs reference script
    Former-commit-id: 29c2ff6a029270042eb154e873e326e8e004f638

--- code/evaluation/confusion.py
+++ code/evaluation/confusion.py
@@ -2,7 +2,9 @@ from os.path import basename, dirname, sep
 from json import load, dumps
 from path import JSON_CORRECT
 
-methods = ["verbatim",
+methods = ["titles",
+           "dois",
+           "pmids",
            "ned <= 0.2",
            "ned <= 0.3",
            "ned <= 0.4",
@@ -26,14 +28,14 @@ for event in events:
                any(event["first_mentioned"][strategies[j]].values()) and \
                min([value["index"] for value in event["first_mentioned"][strategies[j]].values() if value]) < min([value["index"] for value in event["first_mentioned"][strategies[i]].values() if value]):
                 strategy_matrix[i][j] += 1
-    #results = {k:v for k,v in list(event["first_mentioned"]["verbatim"].items()) + list(event["first_mentioned"]["relaxed"].items())}
-    results = {k:v for k,v in list(event["first_mentioned"]["relaxed"].items())}
-    verbatim_methods = [item for item in event["first_mentioned"]["verbatim"].items() if item[1]]
-    if verbatim_methods:
-        best_verbatim = sorted(verbatim_methods, key = lambda item: item[1]["index"])[0]
-        results["verbatim"] = best_verbatim[1]
-    else:
-        results["verbatim"] = None
+    results = {k:v for k,v in list(event["first_mentioned"]["verbatim"].items()) + list(event["first_mentioned"]["relaxed"].items())}
+##    results = {k:v for k,v in list(event["first_mentioned"]["relaxed"].items())}
+##    verbatim_methods = [item for item in event["first_mentioned"]["verbatim"].items() if item[1]]
+##    if verbatim_methods:
+##        best_verbatim = sorted(verbatim_methods, key = lambda item: item[1]["index"])[0]
+##        results["verbatim"] = best_verbatim[1]
+##    else:
+##        results["verbatim"] = None
     for i in range(len(methods)):
         for j in range(len(methods)):
             if results[methods[i]] and results[methods[j]] and results[methods[j]]["index"] < results[methods[i]]["index"]:
