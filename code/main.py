8ed3cc010ec5277916f7a8eeca54075da8b5527c
kircheis <wolfgang.kircheis@uni-leipzig.de>
2021-03-05 01:08:31 +0100

    Updated histor section development samples
    Former-commit-id: 0c0be2b70f1ff8480942ec5cf2c947cf832ce872

--- code/main.py
+++ code/main.py
@@ -1,32 +1,28 @@
 from article.article import Article
 from differ.lcs import Differ
+from difflib import Differ
 import matplotlib.pyplot as plt
 from datetime import datetime
+import numpy as np
 
 article = Article("../articles/2021-03-01/CRISPR_gene_editing_en")
 
 differ = Differ()
 
 timestamps = []
-added = []
-removed = []
+added_characters = []
+removed_characters = []
 sizes = []
 
 ##revid1 = 942550392
 ##revid2 = 942551764
 ##
 ##rev1 = article.get_revision(revid=revid1).section_tree().find(["History"])[0].get_text(6)
-##
 ##rev2 = article.get_revision(revid=revid2).section_tree().find(["History"])[0].get_text(6)
-##
 ##start = datetime.now()
-##
 ##differ.compare(rev1, rev2)
-##
 ##end = datetime.now()
-##
 ##print(end - start)
-##
 ##print(1 + "")
 
 revision_iterator = article.yield_revisions()
@@ -34,7 +30,7 @@ revision_iterator = article.yield_revisions()
 old_string = ""
 revision = next(revision_iterator, None)
 
-while revision.index:
+while revision:
     print(revision.index)
     print(revision.url)
     section_tree = revision.section_tree()
@@ -46,8 +42,15 @@ while revision.index:
     diff = differ.compare(old_string, new_string)
     timestamp = str(revision.timestamp.month).rjust(2,"0") + "/" + str(revision.timestamp.year)
     timestamps.append("" if timestamp in timestamps else timestamp)
-    added.append(len([item for item in diff if item[0] == "+"]))
-    removed.append(len([item for item in diff if item[0] == "-"]))
+    added = 0
+    removed = 0
+    for item in diff:
+        if item[0] == "+":
+            added += 1
+        if item[0] == "-":
+            removed += 1
+    added_characters.append(added)
+    removed_characters.append(removed)
     sizes.append(len(new_string))
     print("="*50)
 
@@ -55,13 +58,14 @@ while revision.index:
 
     revision = next(revision_iterator, None)
 
-plt.figure(figsize=(20, 5), dpi=500)
-plt.subplots_adjust(bottom=0.15, top=0.95, left=0.05, right=0.95)
-plt.tight_layout()
+plt.figure(figsize=(25, 5), dpi=250)
+plt.subplots_adjust(bottom=0.15, top=0.95, left=0.01, right=0.99)
+plt.margins(x=0)
+plt.bar(np.arange(len(added_characters)) - 0.15, added_characters, width=0.3, label="added characters")
+plt.bar(np.arange(len(removed_characters)) + 0.15, removed_characters, width=0.3, label="removed characters")
+plt.plot(list(range(len(sizes))), sizes, label="section size", color="green")
 plt.xticks(list(range(len(timestamps))), timestamps, rotation = 90)
-plt.plot(list(range(len(added))), added, label="added characters")
-plt.plot(list(range(len(removed))), removed, label="removed characters")
-plt.plot(list(range(len(sizes))), sizes, label="section size")
 plt.title("History Section " + article.name)
 plt.legend()
 plt.savefig("History Section " + article.name + ".png")
+
