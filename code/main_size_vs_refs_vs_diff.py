d3b40d0c9e00735db987512f5aa5672141f2b837
kircheis <wolfgang.kircheis@uni-leipzig.de>
2021-03-16 18:17:40 +0100

    Replace f string formatting with concatenation to make code 3.5 compatible for gammaweb
    Former-commit-id: 5cbce48d88b561792ac6094b8a3186f6929d0870

--- code/main_size_vs_refs_vs_diff.py
+++ code/main_size_vs_refs_vs_diff.py
@@ -139,7 +139,7 @@ def plot_size_and_reference_count(timesliced_data, filepath):
     print("Plotting " + articletitle)
 
     reference_counts_color = "y"
-    reference_counts_label = "References in Section " + section_name " in " + articletitle
+    reference_counts_label = "References in Section " + section_name + " in " + articletitle
 
     sizes_color = "b"
     sizes_label = "Size of Section " + section_name + " in " + articletitle
@@ -188,7 +188,7 @@ def plot_diffs(data, filepath, section_name, width, height):
         plt.bar(np.arange(len(removed_characters)) + 0.15, removed_characters, width=0.3, label="removed characters")
         plt.plot(list(range(len(sizes))), sizes, label="section size", color="green")
         plt.xticks(list(range(len(ticks))), ticks, rotation = 90)
-        plt.title("Development of " + section_name " Section in " + articletitle)
+        plt.title("Development of " + section_name + " Section in " + articletitle)
         plt.legend()
         plt.savefig(filepath + "_section_analysis_" + differ_name + ".png")    
     
