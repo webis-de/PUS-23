059de728ca1aa3d3123a7b1e8686054d09abd2c2
Wolfgang Kircheis <wolfgang.kircheis@uni-leipzig.de>
2021-10-04 20:11:24 +0200

    Update dump evaluation script
    Former-commit-id: b968e81a7dda11803d6e88ba4bc1afc54406fa1a

--- code/evaluation/dump.py
+++ code/evaluation/dump.py
@@ -1,69 +1,155 @@
 from glob import glob
 from csv import reader, writer
 from pprint import pformat
+from urllib.parse import quote, unquote
+
+def scroll_to_url(article_name, revid, string):
+    return ("https://en.wikipedia.org/w/index.php?title=" + article_name.replace(" ", "_")
+            + "&oldid=" + revid
+            + "#:~:text=" + quote(string))
 
 filepaths = glob("../../analysis/dump_thorough/*results.csv")
 
 results = {}
+corpus_map = {}
 
-with open("wikipedia_dump_results_collated.csv", "w", newline="") as coalated_file:
-    csv_writer = writer(coalated_file, delimiter=",")
-    for index, filepath in enumerate(filepaths):
-        raise_duplicate = True
-        print(index + 1)
-        with open(filepath) as file:
-            csv_reader = reader(file,  delimiter=',')
-            try:
-                for bibkey, doi_or_pmid, article_name, revid, timestamp, corpus \
-                    in csv_reader:
-                    #csv_writer.writerow([bibkey, doi_or_pmid, article_name, revid, timestamp, corpus])
-                    if article_name not in results:
-                        results[article_name] = {}
-                    if bibkey not in results[article_name]:
-                        results[article_name][bibkey] = {}
-                    if doi_or_pmid not in results[article_name][bibkey]:
-                        results[article_name][bibkey][doi_or_pmid] = set()
-                    else:
-                        pass#print("Duplicate: ", bibkey, doi_or_pmid)
-                    if revid not in results[article_name][bibkey][doi_or_pmid]:
-                        results[article_name][bibkey][doi_or_pmid].add(revid)
-                    else:
-                        if raise_duplicate:
-                            print("Duplicate revid", revid, "in filepath", filepath)
-                            raise_duplicate = False
-            except:
-                print("Error reading", filepath)
+#collect all unique occurrences of (revid,timestamp,corpus) tuples for each article and bibkey
+for index, filepath in enumerate(filepaths):
+    print(index + 1)
+    with open(filepath) as file:
+        csv_reader = reader(file,  delimiter=',')
+        for bibkey, doi_or_pmid, article_name, revid, timestamp, corpus in csv_reader:
+            if article_name not in results:
+                results[article_name] = {}
+            if bibkey not in results[article_name]:
+                results[article_name][bibkey] = {}
+            if doi_or_pmid not in results[article_name][bibkey]:
+                results[article_name][bibkey][doi_or_pmid] = set()
+            results[article_name][bibkey][doi_or_pmid].add((revid,timestamp,corpus))
+            if bibkey not in corpus_map:
+                corpus_map[bibkey] = {"wos":"","accounts":""}
+            for subcorpus in corpus.split("|"):
+                corpus_map[bibkey][subcorpus] = "X"
 
-##article_names_to_delete = set()
-##for article_name, bibkeys in results.items():
-##    delete_article_name = True
-##    for bibkey, dois_or_pmids in bibkeys.items():
-##        for doi_or_pmid, count in dois_or_pmids.items():
-##            if len(dois_or_pmids) == 1 and count == 1:
-##                break
-##            if not doi_or_pmid.isnumeric():
-##                delete_article_name = False
-##                break
-##        if not delete_article_name:
-##            break
-##    if delete_article_name:
-##        article_names_to_delete.add(article_name)
-##
-##for article_name in article_names_to_delete:
-##    del results[article_name]
-
-results = {article_name:results[article_name]#list(list(results[article_name].values())[0].keys())[0]
+#sort articles according to descending number of publications found
+results = {article_name:results[article_name]
            for article_name in sorted(results.keys(),
                                       key=lambda article_name: len(results[article_name]),
                                       reverse=True)}
 
+#for each article and bibkey, find latest revision as per DOI or PMID
+first_and_final_revision_for_article_and_bibkey_map = {}
+for article_name in results:
+    for bibkey in results[article_name]:
+        first_doi_match = ["",""]
+        final_doi_match = ["",""]
+        first_pmid_match = ["",""]
+        final_pmid_match = ["",""]
+        for doi_or_pmid in results[article_name][bibkey]:
+            if doi_or_pmid.isnumeric():
+                revisions_for_pmid_sorted = sorted(results[article_name][bibkey][doi_or_pmid],
+                                                   key=lambda tupl:tupl[1])
+                first_pmid_match = revisions_for_pmid_sorted[0][:2]
+                final_pmid_match = revisions_for_pmid_sorted[-1][:2]
+            else:
+                revisions_for_pmid_sorted = sorted(results[article_name][bibkey][doi_or_pmid],
+                                                   key=lambda tupl:tupl[1])
+                first_doi_match = revisions_for_pmid_sorted[0][:2]
+                final_doi_match = revisions_for_pmid_sorted[-1][:2]
+        if article_name not in first_and_final_revision_for_article_and_bibkey_map:
+            first_and_final_revision_for_article_and_bibkey_map[article_name] = {}
+        first_and_final_revision_for_article_and_bibkey_map[article_name][bibkey] = {"first_pmid_match":first_pmid_match,
+                                                                                     "final_pmid_match":final_pmid_match,
+                                                                                     "first_doi_match":first_doi_match,
+                                                                                     "final_doi_match":final_doi_match}
+                                                                           
+      
+###collate results in results.csv
+##with open("results.csv", "w", newline="") as coalated_file:
+##    csv_writer = writer(coalated_file, delimiter=",")
+##    for article_name in results:
+##        first_time_article_name = True
+##        for bibkey in results[article_name]:
+##            first_time_bibkey = True
+##            for doi_or_pmid in results[article_name][bibkey]:
+##                for revid,timestamp,corpus in results[article_name][bibkey][doi_or_pmid]:
+##                    csv_writer.writerow([article_name if first_time_article_name else "",
+##                                         bibkey if first_time_bibkey else "",
+##                                         doi_or_pmid,
+##                                         revid,
+##                                         timestamp,
+##                                         corpus])
+##                    first_time_article_name = False
+##                first_time_bibkey = False
+
+#map (revid,timestamp,corpus) tuples to count
 for article_name in results:
     for bibkey in results[article_name]:
         for doi_or_pmid in results[article_name][bibkey]:
             results[article_name][bibkey][doi_or_pmid] = len(results[article_name][bibkey][doi_or_pmid])
 
-with open("results.txt", "w") as file:
+
+#sort bibkeys according to descending number of revisions encountered in
+results = {article_name:{bibkey:results[article_name][bibkey]
+                         for bibkey in sorted(results[article_name].keys(),
+                                              key=lambda bibkey: max(results[article_name][bibkey].values()),
+                                              reverse=True)}
+           for article_name in results}
+
+#print to results.txt
+with open("results_basic.txt", "w") as file:
     file.write(pformat(results, sort_dicts=False))
+with open("results_basic.csv", "w") as file:
+    csv_writer = writer(file, delimiter=",")
+    for article_name in results:
+        first_time_article_name = True
+        for bibkey in results[article_name]:
+            pmid = ""
+            pmid_count = ""
+            doi = ""
+            doi_count = ""
+            for doi_or_pmid, count in results[article_name][bibkey].items():
+                if doi_or_pmid.isnumeric():
+                    pmid = doi_or_pmid
+                    pmid_count = count
+                else:
+                    doi = doi_or_pmid
+                    doi_count = count
+            revid_of_first_pmid_match,timestamp_of_first_pmid_match = first_and_final_revision_for_article_and_bibkey_map[article_name][bibkey]["first_pmid_match"]
+            revid_of_final_pmid_match,timestamp_of_final_pmid_match = first_and_final_revision_for_article_and_bibkey_map[article_name][bibkey]["final_pmid_match"]
+            revid_of_first_doi_match,timestamp_of_first_doi_match = first_and_final_revision_for_article_and_bibkey_map[article_name][bibkey]["first_doi_match"]
+            revid_of_final_doi_match,timestamp_of_final_doi_match = first_and_final_revision_for_article_and_bibkey_map[article_name][bibkey]["final_doi_match"]
+            csv_writer.writerow([article_name if first_time_article_name else "",
+                                 bibkey,
+                                 corpus_map[bibkey]["wos"],
+                                 corpus_map[bibkey]["accounts"],
+                                 doi,
+                                 doi_count,
+                                 revid_of_first_doi_match,
+                                 timestamp_of_first_doi_match,
+                                 scroll_to_url(article_name,
+                                               revid_of_first_doi_match,
+                                               doi) if doi else "",
+                                 revid_of_final_doi_match,
+                                 timestamp_of_final_doi_match,
+                                 scroll_to_url(article_name,
+                                               revid_of_final_doi_match,
+                                               doi) if doi else "",
+                                 "---",
+                                 pmid,
+                                 pmid_count,
+                                 revid_of_first_pmid_match,
+                                 timestamp_of_first_pmid_match,
+                                 scroll_to_url(article_name,
+                                               revid_of_first_pmid_match,
+                                               pmid) if pmid else "",
+                                 revid_of_final_pmid_match,
+                                 timestamp_of_final_pmid_match,
+                                 scroll_to_url(article_name,
+                                               revid_of_final_pmid_match,
+                                               pmid) if pmid else "",
+                                 "---"])
+            first_time_article_name = False
             
 ##with open("wikipedia_dump_analysis.txt", "w") as file:
 ##    for article_name in results:
