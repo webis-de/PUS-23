98327ee0f80df925bdd49703d2ea9e7a3a667312
kircheis <kircheis@informatik.uni-leipzig.de>
2021-11-02 18:24:02 +0100

    Update CSV and main_timeline
    Former-commit-id: f65037f596029611fd6950f26386b15726b36fe8

--- code/main_timeline.py
+++ code/main_timeline.py
@@ -2,66 +2,59 @@ from bibliography.bibliography import Bibliography
 from timeline.accountlist import AccountList
 from timeline.eventlist import EventList
 from pprint import pprint
+from glob import glob
 
 ###############################################################
 # This file serves as an entry point view events and accounts.#
 ###############################################################
 
-def heading(text):
-    print(text)
-    print("-"*len(text))
-    print()
-
 if __name__ == "__main__":
 
+    bibliography = Bibliography("../data/CRISPR_literature.csv")
+    accountlist = AccountList("../data/CRISPR_accounts.csv")
+        
     conditions = []
 
     EQUALLING = ["bibentries"]
 
-    for equalling in [[], EQUALLING]:
+    for eventfilename in glob("../data/CRISPR_publication-events*.csv"):
+        print(eventfilename.split("/")[-1] + "\n")
+        for equalling in [[], EQUALLING]:
 
-        if equalling:
-            print("EQULLING OVER",equalling)
-        else:
-            print("NOT EQUALLING EVENTS.")
-
-        bibliography = Bibliography("../data/CRISPR_fieldcorpus_authors.csv")
-        accountlist = AccountList("../data/CRISPR_accounts.csv")
-        eventlist = EventList("../data/CRISPR_publication-events.csv", bibliography, accountlist, conditions, equalling)
-
-        event_types = {}
-
-        for event in eventlist.events:
-            event_type = event.type if event.type else "-"
-            event_extracted_from = event.extracted_from if event.extracted_from else "-"
-            if event_type not in event_types:
-                event_types[event_type] = {}
-            if event_extracted_from not in event_types[event_type]:
-                event_types[event_type][event_extracted_from] = 0
-            event_types[event_type][event_extracted_from] += 1
-
-        heading("\nEVENT TYPES")
-        pprint(event_types, width=10)
-
-        event_count = 0
-
-        for event in eventlist.events:
-            for condition in conditions:
-                if not eval(condition):
-                    break
+            if equalling:
+                print("EQULLING OVER", " AND ".join(equalling))
             else:
-                event_count += 1
+                print("NOT EQUALLING EVENTS.")
 
-        heading("\nEVENT COUNT")
-        print(len(eventlist.events), "(all)\n")
-        print(event_count, "with conditions:")
+            eventlist = EventList(eventfilename, bibliography, accountlist, conditions, equalling)
 
-        print("".join(["\n - " + condition for condition in conditions]))
+            event_types = {}
 
-        heading("\nEVENTS WITH BIBENTRIES")
+            for event in eventlist.events:
+                event_type = event.type if event.type else "-"
+                event_extracted_from = event.extracted_from if event.extracted_from else "-"
+                if event_type not in event_types:
+                    event_types[event_type] = {}
+                if event_extracted_from not in event_types[event_type]:
+                    event_types[event_type][event_extracted_from] = 0
+                event_types[event_type][event_extracted_from] += 1
 
-        print(len([event for event in eventlist.events if event.bibentries]))
+            print("\nEVENT TYPES")
+            pprint(event_types, width=10)
 
-        print("="*100)
+            event_count = 0
+
+            for event in eventlist.events:
+                for condition in conditions:
+                    if not eval(condition):
+                        break
+                else:
+                    event_count += 1
+
+            print(len(eventlist.events), "events,", len([event for event in eventlist.events if event.bibentries]), "events with bibentries")
+
+            if not equalling: print("-"*70)
+
+        print("="*70)
 
         
