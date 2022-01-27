43f28e2a320466c3523020308d04a8a201c9a5e3
kircheis <wolfgang.kircheis@uni-leipzig.de>
2022-01-27 16:37:17 +0100

    Restructure main_candidate.py
    Former-commit-id: 39c00f887faaf1b582fcffccf1075a5f1ea6ae3f

--- code/main_candidate.py
+++ code/main_candidate.py
@@ -229,6 +229,8 @@ def analyse_scrape(article_filepath, timeslices, identifier_map, wos_keys):
 
 if __name__ == "__main__":
 
+    MODE = ["DUMP_CANDIDATES","SCRAPE_ANALYSIS","ARTICLE_PLOT"][1]
+
     test_files = [#"enwiki-20210601-pages-meta-history18.xml-p27121491p27121850.bz2", # 472KB
                   #"enwiki-20210601-pages-meta-history27.xml-p67791779p67827548.bz2", # 25MB
                   #"enwiki-20210601-pages-meta-history21.xml-p39974744p39996245.bz2",   # 150MB
@@ -252,62 +254,6 @@ if __name__ == "__main__":
                '"|".join(dois)) + "|" + ("|".join(pmids)'
                ][0]
 
-##    test = False
-##    quick = False
-##    slice_size = 5
-##
-##    JOB_COMPLETION_INDEX = int(environ.get("JOB_COMPLETION_INDEX"))
-##
-##    output_directory = "../analysis/articles/TEST"
-##    if not exists(output_directory): makedirs(output_directory)
-##
-##    done_filepath = output_directory + sep + "done.csv"
-##    if exists(done_filepath):
-##        with open(done_filepath) as file:
-##            done_input_filepaths = [line.split(",")[0] for line in file.readlines()]
-##    else:
-##        done_input_filepaths = []
-##
-##    if test:
-##        corpus_path_prefix = ("../dumps/")
-##        corpus_path_prefix = "../../../../../" + \
-##                             "corpora/corpora-thirdparty/corpus-wikipedia/wikimedia-history-snapshots/enwiki-20210601/"
-##        input_filepaths = [corpus_path_prefix + input_file for input_file in test_files]
-##    else:
-##        corpus_path_prefix = "../../../../../" + \
-##                             "corpora/corpora-thirdparty/corpus-wikipedia/wikimedia-history-snapshots/enwiki-20210601/"
-##        relevant_bz2_files = []
-##        with open("../analysis/articles/candidates_from_dump/done.csv") as donefile:
-##            for line in donefile:
-##                line = line.split(",")
-##                if line[1] != "0":
-##                    relevant_bz2_files.append(line[0])
-##        input_filepaths = [corpus_path_prefix + relevant_bz2_file for relevant_bz2_file in relevant_bz2_files]
-##
-##    with open("../data/CRISPR_articles_844.txt") as file:
-##        relevant_article_names = set([line.strip() for line in file.readlines()])
-##
-##    identifier_map, wos_map, dois, pmids, wos_keys = get_publication_data_csv()
-##
-##    doi_and_pmid_regex = re.compile(eval(pattern))
-##
-##    for input_filepath_index, input_filepath in enumerate(input_filepaths[JOB_COMPLETION_INDEX*slice_size:(JOB_COMPLETION_INDEX+1)*slice_size], JOB_COMPLETION_INDEX*slice_size):
-##        filesize = getsize(input_filepath)
-##        hostname = gethostname()
-##        log_handler_host(output_directory, hostname, environ.get("NODE_NAME"), input_filepath_index, input_filepath, filesize)
-##        
-##        analyse_dump(input_filepath=input_filepath,
-##                     output_directory=output_directory,
-##                     doi_and_pmid_regex=doi_and_pmid_regex,
-##                     done_input_filepaths=done_input_filepaths,
-##                     article_titles=relevant_article_names,
-##                     quick=quick)
-
-
-    #########################################################################################
-    # The below code is used to analyse the wikitext of scraped articles for DOIs and PMIDs.#
-    #########################################################################################
-
     article_filepaths = sorted(glob("../articles/2021-06-01_no_html/*_en"))
     relevant_article_filepath = [("../data/CRISPR_articles_411.txt", "_411"),
                                  ("../data/CRISPR_articles_844.txt", "_844"),
@@ -318,15 +264,75 @@ if __name__ == "__main__":
     with open(relevant_article_filepath[0]) as file:
         relevant_article_names = set([line.strip() for line in file.readlines()])
 
+    dump_analysis_plot_data_directory = "../analysis/bibliography/2022_01_25"
+    csv_data_filepath = dump_analysis_plot_data_directory + sep + "dump_analysis_plot_data.csv"
+    timeslices = get_timeslices(2001)[:-7]
+
 ##    with open("../analysis/articles/analysis_from_dump/articles_woskeys.txt") as file:
 ##        relevant_wos_keys = [line.strip() for line in file.readlines()]
     relevant_wos_keys = []
-        
-    timeslices = get_timeslices(2001)[:-7]
 
-    dump_analysis_plot_data_directory = "../analysis/bibliography/2022_01_25"
-    csv_data_filepath = dump_analysis_plot_data_directory + sep + "dump_analysis_plot_data.csv"
-    if not exists(csv_data_filepath):
+    ########################################################################################
+    # The below code is used to analyse the wikitext of wikipedia dumps for DOIs and PMIDs.#
+    ########################################################################################
+
+    if MODE == "DUMP_CANDIDATES":
+        test = False
+        quick = False
+        slice_size = 5
+
+        JOB_COMPLETION_INDEX = int(environ.get("JOB_COMPLETION_INDEX"))
+
+        output_directory = "../analysis/articles/TEST"
+        if not exists(output_directory): makedirs(output_directory)
+
+        done_filepath = output_directory + sep + "done.csv"
+        if exists(done_filepath):
+            with open(done_filepath) as file:
+                done_input_filepaths = [line.split(",")[0] for line in file.readlines()]
+        else:
+            done_input_filepaths = []
+
+        if test:
+            corpus_path_prefix = ("../dumps/")
+            corpus_path_prefix = "../../../../../" + \
+                                 "corpora/corpora-thirdparty/corpus-wikipedia/wikimedia-history-snapshots/enwiki-20210601/"
+            input_filepaths = [corpus_path_prefix + input_file for input_file in test_files]
+        else:
+            corpus_path_prefix = "../../../../../" + \
+                                 "corpora/corpora-thirdparty/corpus-wikipedia/wikimedia-history-snapshots/enwiki-20210601/"
+            relevant_bz2_files = []
+            with open("../analysis/articles/candidates_from_dump/done.csv") as donefile:
+                for line in donefile:
+                    line = line.split(",")
+                    if line[1] != "0":
+                        relevant_bz2_files.append(line[0])
+            input_filepaths = [corpus_path_prefix + relevant_bz2_file for relevant_bz2_file in relevant_bz2_files]
+
+        with open("../data/CRISPR_articles_844.txt") as file:
+            relevant_article_names = set([line.strip() for line in file.readlines()])
+
+        identifier_map, wos_map, dois, pmids, wos_keys = get_publication_data_csv()
+
+        doi_and_pmid_regex = re.compile(eval(pattern))
+
+        for input_filepath_index, input_filepath in enumerate(input_filepaths[JOB_COMPLETION_INDEX*slice_size:(JOB_COMPLETION_INDEX+1)*slice_size], JOB_COMPLETION_INDEX*slice_size):
+            filesize = getsize(input_filepath)
+            hostname = gethostname()
+            log_handler_host(output_directory, hostname, environ.get("NODE_NAME"), input_filepath_index, input_filepath, filesize)
+            
+            analyse_dump(input_filepath=input_filepath,
+                         output_directory=output_directory,
+                         doi_and_pmid_regex=doi_and_pmid_regex,
+                         done_input_filepaths=done_input_filepaths,
+                         article_titles=relevant_article_names,
+                         quick=quick)
+
+    #########################################################################################
+    # The below code is used to analyse the wikitext of scraped articles for DOIs and PMIDs.#
+    #########################################################################################
+    
+    if MODE == "SCRAPE_ANALYSIS":
         identifier_map, wos_map, dois, pmids, wos_keys = get_publication_data_csv(relevant_wos_keys)
         doi_and_pmid_regex = re.compile(eval(pattern))
         
@@ -339,7 +345,12 @@ if __name__ == "__main__":
             for line in lines:
                 csv_writer.writerow(line)
             csv_writer.writerow([""] + timeslices)
-    else:
+
+    ##############################################################################################
+    # The below code is used to plot the publication distribution of the scrape analysis results.#
+    ##############################################################################################
+
+    if MODE == "ARTICLE_PLOT":
         lines = []
         with open(csv_data_filepath) as csvfile:
             csv_reader = csv.reader(csvfile, delimiter=",")
@@ -347,76 +358,72 @@ if __name__ == "__main__":
                 if row[0] in relevant_article_names:
                     lines.append(row)
 
-    ####################################################################################
-    # The below code is used to plot the publication distribution of the above results.#
-    ####################################################################################
-
-    # sort results according to max number of publications in any timeslice        
-    bibkey_max_per_month_count_sort_map = {line[0]:max([int(item.split("/")[-1]) for item in line[1:]])
-                                           for line in lines}
-    lines = sorted(lines, key=lambda line: bibkey_max_per_month_count_sort_map[line[0]])
-    
-    start_index = 1
-    for i in range(1, len(lines[0])):
-        if any([line[i] != "0" for line in lines]):
-            start_index = i
-            break
-    start_index -= 1
-
-    timeslices = timeslices[start_index-1:]
-    lines = [[line[0]] + line[start_index:] for line in lines]
-       
-    #cm = mcol.LinearSegmentedColormap.from_list("MyCmapName",["black","r"])    
-    fig, ax = plt.subplots()
-    fig.set_dpi(150.0)
-    height = int(len(lines)/4)
-    width = len(timeslices[start_index:])/10
-    fig.set_figheight(height)
-    fig.set_figwidth(width)          
-
-    legend = None
-    
-    for line in lines:
-        data = {'x': timeslices,
-                'y': [line[0].replace("(Scarless Cas9 Assisted Recombineering)",
-                                      "[...]").replace("knockout screens", "[...]") for _ in timeslices],
-                'd': [float(item.split("/")[-1]) for item in line[1:]]}
+        # sort results according to max number of publications in any timeslice        
+        bibkey_max_per_month_count_sort_map = {line[0]:max([int(item.split("/")[-1]) for item in line[1:]])
+                                               for line in lines}
+        lines = sorted(lines, key=lambda line: bibkey_max_per_month_count_sort_map[line[0]])
         
-        scatter = ax.scatter('x', 'y', s='d', c='black', data=data, zorder=1, marker=((0,-5),(0,5)), linewidth=1)
+        start_index = 1
+        for i in range(1, len(lines[0])):
+            if any([line[i] != "0" for line in lines]):
+                start_index = i
+                break
+        start_index -= 1
 
-        if line[0] == "CRISPR":
-            handles, labels = scatter.legend_elements(prop="sizes", alpha=0.6)
-            handles = [handles[0], handles[3], handles[-1]]
-            labels = ["25","100","200"]
-            legend = ax.legend(handles, labels, ncol=3, labelspacing=1, handletextpad=0, loc="lower left", title="Number of Publications")
+        timeslices = timeslices[start_index-1:]
+        lines = [[line[0]] + line[start_index:] for line in lines]
+           
+        #cm = mcol.LinearSegmentedColormap.from_list("MyCmapName",["black","r"])    
+        fig, ax = plt.subplots()
+        fig.set_dpi(150.0)
+        height = int(len(lines)/4)
+        width = len(timeslices[start_index:])/10
+        fig.set_figheight(height)
+        fig.set_figwidth(width)          
+
+        legend = None
         
-    ax.set(xlabel='', ylabel='')
-    ax.tick_params(axis='x', labelsize=10.0, labelrotation=90)
-    ax.tick_params(axis='y', labelsize=10.0)
-    ax.set_xticklabels([timeslice if index % 6 == 0 else "" for index,timeslice in enumerate(timeslices)])
-    ax.margins(x=0.01, y=0.35/height)
-    
-    adjustment_left = 2.5/width
-    adjustment_right = 0.995
-    adjustment_bottom = 0.75/height
-    adjustment_top = 0.995
-    
-    plt.subplots_adjust(left=adjustment_left,
-                        right=adjustment_right,
-                        bottom=adjustment_bottom,
-                        top=adjustment_top)
+        for line in lines:
+            data = {'x': timeslices,
+                    'y': [line[0].replace("(Scarless Cas9 Assisted Recombineering)",
+                                          "[...]").replace("knockout screens", "[...]") for _ in timeslices],
+                    'd': [float(item.split("/")[-1]) for item in line[1:]]}
+            
+            scatter = ax.scatter('x', 'y', s='d', c='black', data=data, zorder=1, marker=((0,-5),(0,5)), linewidth=1)
 
-    print("articles:", relevant_article_filepath[1].replace("_", " ").strip())
-    print("height:", height, "width:", width)
-    print("\n\t".join(["adjustments:",
-                     "left: " + str(adjustment_left),
-                     "right: " + str(adjustment_right),
-                     "bottom: " + str(adjustment_bottom),
-                     "top: " + str(adjustment_top)]))
+            if line[0] == "CRISPR":
+                handles, labels = scatter.legend_elements(prop="sizes", alpha=0.6)
+                handles = [handles[0], handles[3], handles[-1]]
+                labels = ["25","100","200"]
+                legend = ax.legend(handles, labels, ncol=3, labelspacing=1, handletextpad=0, loc="lower left", title="Number of Publications")
+            
+        ax.set(xlabel='', ylabel='')
+        ax.tick_params(axis='x', labelsize=10.0, labelrotation=90)
+        ax.tick_params(axis='y', labelsize=10.0)
+        ax.set_xticklabels([timeslice if index % 6 == 0 else "" for index,timeslice in enumerate(timeslices)])
+        ax.margins(x=0.01, y=0.35/height)
+        
+        adjustment_left = 2.5/width
+        adjustment_right = 0.995
+        adjustment_bottom = 0.75/height
+        adjustment_top = 0.995
+        
+        plt.subplots_adjust(left=adjustment_left,
+                            right=adjustment_right,
+                            bottom=adjustment_bottom,
+                            top=adjustment_top)
 
-    transparent = False
-    
-    plt.savefig(dump_analysis_plot_data_directory + sep + ("transparent_" if transparent else "") + "plot" + relevant_article_filepath[1] + ".png",
-                transparent=transparent)
+        print("articles:", relevant_article_filepath[1].replace("_", " ").strip())
+        print("height:", height, "width:", width)
+        print("\n\t".join(["adjustments:",
+                         "left: " + str(adjustment_left),
+                         "right: " + str(adjustment_right),
+                         "bottom: " + str(adjustment_bottom),
+                         "top: " + str(adjustment_top)]))
+
+        transparent = False
+        
+        plt.savefig(dump_analysis_plot_data_directory + sep + ("transparent_" if transparent else "") + "plot" + relevant_article_filepath[1] + ".png",
+                    transparent=transparent)
             
 
