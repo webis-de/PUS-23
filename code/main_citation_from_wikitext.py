a3c7e70b3b7476bd6ae16f56e84fc313b29968c5
kircheis <wolfgang.kircheis@uni-leipzig.de>
2023-03-17 15:28:37 +0100

    Update citation extraction prototype
    Former-commit-id: 262c8b4535e0d73bc7419d395876ba89d00d73fe

--- code/main_citation_from_wikitext.py
+++ code/main_citation_from_wikitext.py
@@ -1,32 +1,76 @@
 from wikidump.wikitext_reader import WikitextReader
+from wikidump.wikipedia_dump_reader import WikipediaDumpReader
 from json import load
 from pprint import pprint
 from datetime import datetime
 from regex import search
+from csv import writer
+from numpy import mean, std
 
-with open("/mnt/Data/uni/09THESIS/ba-kircheis-wikipedia-code/corpora/2022_03_29_01_39_26/science_and_technology_corpus.json") as file:
-    corpus = load(file)
+analyse = True
+verbose = False
+doi_map = {}
 
-for entry in corpus:
-    wtr = WikitextReader(entry["article_title"],
-                         entry["pageid"],
-                         entry["revid"],
-                         entry["timestamp"],
-                         entry["wikitext"])
-    revision_year = datetime.strptime(wtr.timestamp, "%Y-%m-%dT%H:%M:%SZ").year
-    for reference in wtr.references():
-        if reference.get("doi", None):
-            pprint(reference)
-            if reference.get("year", None) and len(reference["year"]) == 4:
-                reference_year = int(reference["year"])
-            elif reference.get("date", None):
-                match = search("\d\d\d\d", reference["date"])
-                if match and len(match.group()) == 4:
-                    reference_year = int(match.group())
-            else:
-                reference_year = None
-            print(reference["title"])
-            print("\tReference year:", reference_year)
-            print("\tRevision year:", revision_year)
-            print("-"*50)
-    input("="*50)
+with open("../analysis/citations/citations_from_wikitext_test.csv", "w") as citation_file:
+    with open("../analysis/citations/latency_from_wikitext_test.csv", "w") as latency_file:
+        citation_csv_writer = writer(citation_file, delimiter="|")
+        latency_csv_writer = writer(latency_file, delimiter="|")
+        citation_csv_writer.writerow(["title", "title_year", "revision_index", "revid", "doi", "reference_year", "revision_year", "citation_latency"])
+        latency_csv_writer.writerow(["title", "pageid", "number of dois", "mean citation latency", "standard deviation"])
+        with WikipediaDumpReader(("/media/wolfgang/Data/Work/git/code-research/computational-social-science/" +
+                                  "science-analytics-wikipedia/dumps/enwiki-20230301-pages-meta-history10.xml-p5382015p5399366.bz2")) as wdr:
+            old_title = None
+            revision_index = 0
+            title_year = None
+            latencies = []
+            for title,pageid,revid,timestamp,text in wdr.line_iter():
+                if title not in doi_map:
+                    doi_map[title] = set()
+                if title != old_title:
+                    if len(latencies) >= 10:
+                        latency_csv_writer.writerow([title, pageid, len(latencies), round(mean(latencies), 2), round(std(latencies), 2)])
+                        latency_file.flush()
+                        latencies = []
+                    old_title = title
+                    title_year = None
+                    revision_index = 0
+                if analyse:
+                    doi_found = False
+                    wtr = WikitextReader(title,pageid,revid,timestamp,text)
+                    if verbose: print(title, revid, timestamp)
+                    revision_year = datetime.strptime(wtr.timestamp, "%Y-%m-%dT%H:%M:%SZ").year
+                    if not title_year:
+                        title_year = revision_year
+                    for reference in wtr.references():
+                        doi = reference.get("doi", None)
+                        if doi:
+                            doi_found = True
+                            if verbose: print("-"*50)
+                            if verbose: pprint(reference, sort_dicts=False)
+                            if reference.get("year", None) and len(reference["year"]) == 4:
+                                reference_year = int(reference["year"])
+                            elif reference.get("date", None):
+                                match = search("\d\d\d\d", reference["date"])
+                                if match and len(match.group()) == 4:
+                                    reference_year = int(match.group())
+                            else:
+                                reference_year = None
+                            if doi not in doi_map[title]:
+                                doi_map[title].add(doi)
+                                if title_year and revision_year and reference_year:
+                                    citation_latency = revision_year - max([reference_year, title_year])
+                                    latencies.append(citation_latency)
+                                    citation_csv_writer.writerow([title, title_year,
+                                                                  revision_index, revid,
+                                                                  doi, reference_year, revision_year, citation_latency])
+                                    citation_file.flush()
+                            if verbose: print(reference["title"])
+                            if verbose: print("\tReference year:", reference_year)
+                            if verbose: print("\tRevision year:", revision_year)
+                         
+                    if verbose: print("="*50)
+                    if doi_found:
+                        if verbose: input()
+                        doi_found = False
+
+                revision_index += 1   
